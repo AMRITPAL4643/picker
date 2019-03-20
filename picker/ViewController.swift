0528f8d478f35e9ff2dfd6b0a64148bf90a21bd9
Amrit <admin@admins-MacBook-Pro.local>
2019-03-20 16:07:04 -0400

    picker

--- picker/ViewController.swift
+++ picker/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    
+
+    @IBOutlet weak var countrypicker: UIPickerView!
+    @IBOutlet weak var labelfield: UILabel!
+    
+    let country = ["A","B","C","D"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        countrypicker.delegate = self
+        countrypicker.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.country.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.country[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.labelfield.text = country[countrypicker.selectedRow(inComponent: 0)]
+    }
 
 }
 
